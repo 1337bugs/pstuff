4170281b9a20b48bb072f0137011708b1955edc1
1337bugs <34063942+1337bugs@users.noreply.github.com>
2020-05-27 18:01:32 +0300

    Finished task
    Added last get_student_average function, cleaned the code, added comments

--- task2/task2_solution.py
+++ task2/task2_solution.py
@@ -1,3 +1,15 @@
+# NOTES:
+#
+# no idea whether it's ok to make
+# to_letters function a method of the class
+# (staticmethod doesn't work)
+# external func works fine, let is be so
+#
+# besides that, Journal class feels to be
+# not really universal (adding more fields to
+# input data requires changes in code)
+
+
 def to_letters(grades):
     res = ""
     for grade in grades:
@@ -17,6 +29,13 @@ def to_letters(grades):
     return res.strip()
 
 
+# calc overall average grade
+def avg_grade(homework, quizzes, tests):
+    grades = homework + quizzes + tests
+    res = sum(grades)/len(grades)
+    return res
+
+
 class Journal:
     student_data = []
 
@@ -24,27 +43,25 @@ class Journal:
         for dict in students:
             self.student_data.append(dict)
 
-
     def get_letter_grades(self, student_name):
         for dict in self.student_data:
             if student_name in dict["name"]:
-                print("Grades of " + student_name +": ")
+                print("Grades of " + student_name + ": ")
                 print("Homework:", to_letters(dict["homework"]))
                 print("Quizzes:", to_letters(dict["quizzes"]))
                 print("Tests:", to_letters(dict["tests"]))
-                print()
 
     def get_student_average(self, student_name):
-        pass
-
-
-
-
-
-
-    def print_data(self):
-        for item in self.student_data:
-            print(item)
+        for dict in self.student_data:
+            if student_name in dict["name"]:
+                print("Average grade of", student_name, ":", "{0:5.2f}".format(
+                    avg_grade(dict["homework"],
+                              dict["quizzes"],
+                              dict["tests"])))
+    # printing data, for testing
+    # def print_data(self):
+        # for item in self.student_data:
+            # print(item)
 
 
 def main():
@@ -52,9 +69,9 @@ def main():
     students = [
         {
           "name": "Lloyd",
-          "homework": [90.0,97.0,75.0,92.0],
-          "quizzes": [88.0,40.0,94.0],
-          "tests": [75.0,90.0]
+          "homework": [90.0, 97.0, 75.0, 92.0],
+          "quizzes": [88.0, 40.0, 94.0],
+          "tests": [75.0, 90.0]
         },
         {
           "name": "Alice",
@@ -71,18 +88,22 @@ def main():
     ]
 
     journal = Journal(students)
-    #journal.print_data()
     print()
 
-    #testing to_letters
+    # testing to_letters
     #
-    #sample = [100.0, 92.0, 98.0, 100.0]
-    #res = ""
-    #res += to_letters(sample)
-    #print(res)
+    # sample = [100.0, 92.0, 98.0, 100.0]
+    # res = ""
+    # res += to_letters(sample)
+    # print(res)
 
     journal.get_letter_grades("Alice")
+    journal.get_student_average("Alice")
 
+    journal.get_letter_grades("Tyler")
+    journal.get_student_average("Tyler")
+
+    journal.get_student_average("Lloyd")
 
 
 if __name__ == "__main__":
